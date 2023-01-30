c459585b39f7ed80b89781873186036a442414ee
samsleepingatparty <itstaurens@gmail.com>
2023-01-30 16:16:49 +0700

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,41 @@
-import './globals.css'
+import Image from "next/image";
+import Link from "next/link";
+import '../styles/globals.css';
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const header = (
+    <header>
+      <div className="text-center bg-slate-800 p-8 my-6 rounded-md">
+        <Image src="/next.svg" width={40} height={40} className="mx-auto invert" alt={"logo"} />
+        <Link href="/">
+          <h1 className="text-2xl text-white font-bold mt-4">XEM's Blog</h1>
+        </Link>
+        <p className="text-slate-300">Welcome to my daily blog</p>
+      </div>
+    </header>
+  );
+
+  const footer = (
+    <footer>
+      <div className="border-t border-slate-400 mt-12 py-6 text-center text-slate-900">
+        <h3>Designed by XEM</h3>
+      </div>
+    </footer>
+  );
+
   return (
-    <html lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
-      <head />
-      <body>{children}</body>
+    <html>
+      <body>
+        <div className="mx-auto max-w-2xl px-6">
+          {header}
+          {children}
+          {footer}
+        </div>
+      </body>
     </html>
   )
 }
